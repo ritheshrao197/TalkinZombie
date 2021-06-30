15cf629e4ef4a136f984c129a3b0d0ddfec6e92d
rithesh <rithesh197gmail@gmail.com>
2021-06-30 19:06:30 +0530

    Added All files

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField] Slider picthSlider;
+    [SerializeField] TextMeshProUGUI pitchvalue;
+
+    public void OnPitchValueChanged()
+    {
+        pitchvalue.text = picthSlider.value.ToString();
+        if (GameController.OnPitchValueChanged!=null)
+    GameController.OnPitchValueChanged(picthSlider.value);
+    }
+    private void OnEnable()
+    {
+        pitchvalue.text = picthSlider.value.ToString();
+
+    }
+
+ 
+}
